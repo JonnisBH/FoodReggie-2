0ea78ee3cdbab0c8bc0adc30c12500dfa0c14eda
Jonas Haugvaldstad <jonasbhaug@hotmail.com>
2024-12-01 18:55:06 +0100

    Installert bootstrap og lagt til en basic navbar

--- src/home/HomePage.tsx
+++ src/home/HomePage.tsx
@@ -1,22 +1,24 @@
 import React from "react";
+import Button from 'react-bootstrap/Button';
 //import API_URL from "../apiConfig";
 
 const HomePage: React.FC = () => {
+    
     return(
-        <div className="tw-outline tw-outline-1 tw-outline-offset-0 tw-outline-gray-300 tw-mx-28 tw-my-6 tw-shadow-xl tw-rounded-lg">
+        <div className="tw-outline tw-outline-1 tw-outline-offset-0 tw-outline-gray-300 tw-mx-28 tw-my-6 tw-shadow-2xl tw-rounded-lg">
             <div className="tw-flex tw-w-full tw-h-screen">
-                <div id="backgroundAnimated" className="tw-basis-4/6 tw-flex tw-flex-col tw-space-y-3 tw-justify-center tw-items-start tw-backdrop-invert tw-rounded-l-lg">
+                <div className="tw-basis-4/6 tw-flex tw-flex-col tw-space-y-3 tw-justify-center tw-items-start tw-backdrop-invert tw-rounded-l-lg">
                     <h1 className="tw-mx-40 tw-text-6xl tw-font-bold tw-text-white">Welcome to our FoodReggie!</h1>
                     <p className="tw-mx-40 tw-text-xl tw-text-white">
                         A food registration tool to help you track any food of your choice.
-                        <a className="tw-underline tw-decoration-[#258cfb]" href="/foods">Create, read, update and delete </a>
+                        <p className="tw-underline tw-decoration-[#258cfb]">Create, read, update and delete </p>
                         within a single page application.
                     </p>
                 </div>
 
                 <div className="tw-basis-2/6 tw-flex tw-flex-col tw-space-y-5 tw-justify-center tw-items-center">
                     <h1 className="tw-text-4xl tw-font-bold">Navigation</h1>
-                    <a type="button" href="/foods">View tool</a>
+                    <Button variant="primary" size="lg" href="/foods">View tool</Button>
                 </div>
             </div>
         </div>
---
+++ src/nav/NavMenu.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import {Nav, Navbar} from "react-bootstrap";
+import Button from 'react-bootstrap/Button';
+
+const NavMenu: React.FC = () => {
+    return (
+        <Navbar expand="lg" className="tw-flex">
+            <Navbar.Brand href="/"><img src={"/images/carrot-solid.svg"} width="30" height="30" alt="" />FOODREGGIE</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto">
+                    <Nav.Link href="/"><Button variant="primary">Midlertidig</Button></Nav.Link>
+                </Nav>
+            </Navbar.Collapse>    
+        </Navbar>
+        
+    );
+};
+
+export default NavMenu;
